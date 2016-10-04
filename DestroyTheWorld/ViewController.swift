d3e29327e063fee412c46d43cd3d36ec2b7f97f9
Michael Fisher <michaelfisher@michaels-macbook-pro.local>
2016-10-04 11:19:53 +0100

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Michael Fisher on 04/10/2016.
+//  Copyright © 2016 Michael Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
